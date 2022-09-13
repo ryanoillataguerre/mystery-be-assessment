f99a4eeeab9925627b63f3a0db98304978f48da8
Ryan Oillataguerre <ryanoillataguerre@gmail.com>
2022-09-13 13:19:49 -0700

    Clean up migration down function

--- services/api-service/src/db/migrations/initial_schema.ts
+++ services/api-service/src/db/migrations/initial_schema.ts
@@ -45,5 +45,8 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-	await knex.schema.dropTableIfExists("users");
+	await knex.schema
+		.dropTableIfExists("users")
+		.dropTableIfExists("loan_applications")
+		.dropTableIfExists("loan_offers");
 }
